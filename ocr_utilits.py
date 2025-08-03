5a2ca368f3aa01cf29c22288a3fe67821f09bbaf
Karolisas930 <kiupeliskarolis475@googlemail.com>
2025-08-03 18:27:33 +0100

    Update ocr_utilits.py

--- ocr_utilits.py
+++ ocr_utilits.py
@@ -4,8 +4,3 @@ import pytesseract
 def extract_text_from_image(image_path):
     image = Image.open(image_path)
     return pytesseract.image_to_string(image)
-
-cd Clip-pdf-classifier
-git add ocr_utils.py test_ocr.py
-git commit -m "Add OCR utility and test script"
-git push origin main # or `master` depending on branch
